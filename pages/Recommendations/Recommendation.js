924f51a7efe3866d83afe6a23aab364a81a9340d
patilunnati43 <patilunnati43@gmail.com>
2022-09-06 20:21:56 +1000

    Add new pages

---
+++ pages/Recommendations/Recommendation.js
@@ -0,0 +1,16 @@
+export default function recommendation() {
+    return <div>
+        <h1>3 things I would be most interested to do</h1>
+        <ul>
+            <li>Work with the Team to make sure email tone is consistent with the Vercel brand.</li>
+            <li>Find ways to continually improve the day-to-day workflow for converting leads.</li>
+            <li>Hop on a Zoom call to qualify a prospect or educate them about Vercel.</li>
+        </ul>
+        <h1>3 things I would be least interested to do</h1>
+        <ul>
+            <li>Evaluate OSS sponsorship applications.</li>
+            <li>Evaluate an application to help determine if it's commercial or not.</li>
+            <li>Attend weekly enablement meetings to level up your product and sales knowledge.</li>
+        </ul>
+    </div>
+}
